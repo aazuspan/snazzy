49276b7aa16db146092a8ab68676b37a1b3ffb94
aa.zuspan <aa.zuspan@gmail.com>
2022-04-18 02:04:34 +0000

    Updated tests/test.js (added 29 lines, deleted 35 lines).

--- tests/test.js
+++ tests/test.js
@@ -1,59 +1,53 @@
-var testing = require("users/aazuspan/tests:tests");
+var eet = require("users/aazuspan/eet:eet");
 var snazzy = require("users/aazuspan/snazzy:styles");
 
-var tests = {};
+eet.test("Style asset is public", function() {
+  var style = snazzy.styleAsset;
+  var acl = ee.data.getAssetAcl(style);
+  eet.assert.strictEqual(
+    acl["all_users_can_read"], true, "Collection is not public!"
+  );
+})
 
-tests.testCollectionIsPublic = function() {
-    var style = snazzy.styleAsset;
-    var acl = ee.data.getAssetAcl(style);
-    testing.assert.strictEqual(
-      acl["all_users_can_read"], true, "Collection is not public!"
-    );
-}
-
-tests.testStyleFromName = function() {
+eet.test("Add style by name", function() {
   snazzy.addStyleFromName("Retro");
-}
+})
 
-tests.testStyleFromURL = function() {
+eet.test("Add style by URL", function() {
   snazzy.addStyle("https://snazzymaps.com/style/8097/wy");
-}
+})
 
-tests.testMultipleStyles = function() {
+eet.test("Add multiply styles by URL", function() {
   var styles = {
       "https://snazzymaps.com/style/13/neutral-blue": null,
       "https://snazzymaps.com/style/15/subtle-grayscale": null
   };
   snazzy.addStyles(styles);
-}
+})
 
-tests.testStyleFromTags = function() {
+eet.test("Add style from tags", function() {
   snazzy.addStyleFromTags(["colorful", "light"], "rand", "random");  
-}
+})
 
-// Test that an invalid style URL throws an error
-tests.testBadUrl = function() {
+eet.test("Invalid URL throws error", function() {
   var func = function() {snazzy.addStyle("https://invalidurl.biz")};
-  testing.assert.throws(func, Error, new RegExp("could not be found"));
-}
+  eet.assert.throws(func, Error, new RegExp("could not be found"));
+})
 
-// Test that an invalid style name throws an error
-tests.testBadName = function() {
+eet.test("Invalid name throws error", function() {
   var func = function() {snazzy.addStyleFromName("SDJfDSJFKdjfKDSLJF394ujFDK0f2")};
-  testing.assert.throws(func, Error, new RegExp("could not be found"));
-}
+  eet.assert.throws(func, Error, new RegExp("could not be found"));
+})
 
-// Test that an invalid tag throws an error
-tests.testBadTag = function() {
+eet.test("Invalid tag throws error", function() {
   var func = function() {snazzy.addStyleFromTags(["invalid-tag"])};
-  testing.assert.throws(func, Error, new RegExp("not a recognized tag"));
-}
+  eet.assert.throws(func, Error, new RegExp("not a recognized tag"));
+})
 
-// Test that an invalid sort order throws an error
-tests.testBadOrder = function() {
+eet.test("Invalid sort order throws error", function() {
   var func = function() {snazzy.addStyleFromTags(["colorful"], null, "invalid")};
-  testing.assert.throws(func, Error, new RegExp("Order should be"));
-}
+  eet.assert.throws(func, Error, new RegExp("Order should be"));
+})
 
 
-testing.runTests(tests);
+eet.run();
